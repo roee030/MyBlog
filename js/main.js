f8f36ee7080fea2ccbd26984ad07ffa0b6705d6b
roee030 <roee.angel93@gmail.com>
2020-07-17 19:03:35 +0300

    add blog section

--- js/main.js
+++ js/main.js
@@ -7,4 +7,15 @@ $(document).ready(function () {
     $toggleCollapse.click(() => {
         $nav.toggleClass('collapse');
     })
+
+    $('.owl-carousel').owlCarousel(
+        {
+            loop:true,
+            autoplay:false,
+            autoplayTimeout:2000,
+            dots:false,
+            nav:true,
+            navText:[$('.owl-nav-prev') ,$('.owl-nav-next')]
+        }
+    );
 })
